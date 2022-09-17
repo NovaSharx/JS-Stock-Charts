2997c3f9e9c867b0bf679424b66bbc7209a97458
Davin <davin-dj@hotmail.com>
2022-09-17 09:10:30 -0400

    Completed activity

--- public/index.js
+++ public/index.js
@@ -13,7 +13,65 @@ async function main() {
     })
 
     let jsonResponse = await response.json()
-    console.log(jsonResponse)
+    
+    // const {GME, MSFT, DIS, BTNX} = jsonResponse
+    const {GME, MSFT, DIS, BNTX} = mockData
+    const stocks = [GME, MSFT, DIS, BNTX] 
+
+    stocks.forEach(stock => stock.values.reverse())
+
+    console.log(stocks)
+
+    new Chart(timeChartCanvas.getContext('2d'), {
+        type: 'line',
+        data: {
+            labels: stocks[0].values.map(value => value.datetime),
+            datasets: stocks.map(stock => ({
+                label: stock.meta.symbol,
+                data: stock.values.map(value => parseFloat(value.high)),
+                backgroundColor: 
+                    getColor(stock.meta.symbol),
+                borderColor:
+                    getColor(stock.meta.symbol),
+            }))
+        }
+    });
+
+    new Chart(highestPriceChartCanvas.getContext('2d'), {
+        type: 'bar',
+        data: {
+            labels: stocks.map(stock => stock.meta.symbol),
+            datasets: [{
+                label: 'Highest',
+                data: stocks.map(stock => getHighest(stock.values)),
+                backgroundColor: stocks.map(stock => getColor(stock.meta.symbol)),
+                borderColor: stocks.map(stock => getColor(stock.meta.symbol))
+            }]
+        }
+    });
+
+    function getColor (stock) {
+        if (stock === 'GME') {
+            return 'rgba(61, 161, 61, 0.7)'
+        }
+        if (stock === 'MSFT') {
+            return 'rgba(209, 4, 25, 0.7)'
+        }
+        if (stock === 'DIS') {
+            return 'rgba(18, 4, 209, 0.7)'
+        }
+        if (stock === 'BNTX') {
+            return 'rgba(166, 43, 158, 0.7)'
+        }
+    }   
+    
+    function getHighest(values) {
+        let highest = 0
+        values.forEach(value => {
+            if (value.high > highest) {highest = value.high}
+        })
+        return highest
+    }
 
 }
 
