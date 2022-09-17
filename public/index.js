6755d1148a2c37413dfc34c2ff15be27969670bd
Davin <davin-dj@hotmail.com>
2022-09-17 01:10:44 -0400

    filtered stock symbols

--- public/index.js
+++ public/index.js
@@ -4,6 +4,17 @@ async function main() {
     const highestPriceChartCanvas = document.querySelector('#highest-price-chart');
     const averagePriceChartCanvas = document.querySelector('#average-price-chart');
 
+    let response = await fetch('https://api.twelvedata.com/time_series?symbol=BNTX,DIS,GME,MSFT&interval=1min&apikey=66e6e17a0edb42b29de0a8651b6d7c63', {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            Authorization: 'apikey 66e6e17a0edb42b29de0a8651b6d7c63'
+        }
+    })
+
+    let jsonResponse = await response.json()
+    console.log(jsonResponse)
+
 }
 
 main()
